b390be20abd45d0d3fc8d5a43355d285381bc073
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2023-11-19 00:44:03 -0800

    Final- Added Favicon and Website title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import Header from '@/components/Header'
 import MiddleBar from '@/components/MiddleBar'
 import FoodSection from '@/components/FoodSection'
@@ -5,6 +6,12 @@ import FoodSection from '@/components/FoodSection'
 const Home = () => {
   return (
     <main>
+      
+        <Helmet>
+          <title>Oltre 20.000 ricette veloci, semplici e gustose | Al.ta Cucina</title>
+          <link rel="icon" href="/favicon-96x96.png" />
+        </Helmet>
+
         <Header />
         <MiddleBar />
         <FoodSection />
